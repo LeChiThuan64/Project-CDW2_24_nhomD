5a789e9baf6c703487d0984a85ccba1e57c98e06
huuhoang0601 <jinpaxencode@gmail.com>
2024-11-04 19:43:28 +0700

    update func ElasticSearch, Pagination, filter"

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -49,6 +49,5 @@ class LoginController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
-        return redirect('/'); // replace with your desired logout redirect
     }
 }
